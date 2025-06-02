1791d6199272006c21af204fa753cf2986b50773
leleo001 <leonardo213prestes@gmail.com>
2025-06-02 08:04:17 -0300

    Update lista02.js

--- lista02.js
+++ lista02.js
@@ -4,17 +4,21 @@ const furiaCS = ["FalleN","chelo","skullz","yuurih","KSCERATO"];
 const cheloIndex = furiaCS.indexOf("chelo");
 console.log("Índice de chelo:", cheloIndex);
 
+
 // 2) Usando .splice, substitua "chelo" e "skullz" por "molodoy" e "YEKINDAR"
 furiaCS.splice(cheloIndex, 2, "molodoy", "YEKINDAR");
 console.log("Lista após substituição:", furiaCS);
+
 
 // 3) Usando .map, crie uma lista com os caracteres maiúsculos (usar .toUpperCase)
 const upperCaseList = furiaCS.map(player => player.toUpperCase());
 console.log("Lista com nomes em maiúsculo:", upperCaseList);
 
+
 // 4) Usando .sort, ordene a lista com os nomes maiúsculos
 const sortedList = upperCaseList.sort();
 console.log("Lista ordenada em maiúsculo:", sortedList);
+
 
 // 5) Usando .filter, filtre apenas os nomes que começam com "y"
 const yPlayers = furiaCS.filter(player => player.toLowerCase().startsWith("y"));
